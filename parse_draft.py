24e69e5fa94c57e45f22f9f7773fe9c1d45c1c2d
Al Scherer <alscherer@gmail.com>
2024-01-24 17:16:25 -0600

    Updated parse_draft

--- parse_draft.py
+++ parse_draft.py
@@ -4,6 +4,8 @@
     Reduce the draft data to a manageable dict
     Result is a dict - key is the player_id, list of transactions
 
+    Maybe combine the downloads (get-...) with this work but we'll see.
+
     Written by: Al Scherer   2022-01-14
 """
 
@@ -12,8 +14,8 @@ import json
 import sys
 import datetime
 
-DRAFT = 'data/2022/draft-2022.json'
-DRAFT_DATE = 'Sep 1, 2022'
+DRAFT = 'data/2023/draft-2023.json'
+DRAFT_DATE = 'Aug 28, 2023'
 
 # ---------------------------------------------------------
 def get_args():
